ded49f7ae6a67d71d428771bbf6ad42d949b2a96
Silvio Silva <smsilva@gmail.com>
2021-09-04 16:45:37 -0300

    Configurações iniciais

--- src/provider.tf
+++ src/provider.tf
@@ -4,7 +4,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.74.0"
+      version = "= 2.74.0"
     }
   }
+}
+
+provider "azurerm" {
+  features {}
 }
