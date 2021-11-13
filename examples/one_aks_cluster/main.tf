874b175eb2dd4892fe2099ffa7110ef5b849476e
Silvio Silva <smsilva@gmail.com>
2021-11-13 10:00:52 -0300

    WIP

--- examples/one_aks_cluster/main.tf
+++ examples/one_aks_cluster/main.tf
@@ -1,30 +1,30 @@
 locals {
+  name                    = "wasp-aks"
   location                = "centralus"
   admin_group_ids         = ["d5075d0a-3704-4ed9-ad62-dc8068c7d0e1"]
-  virtual_network_name    = "vnet-private"
+  virtual_network_name    = "${local.name}-vnet"
   virtual_network_cidrs   = ["10.0.0.0/8"]
   virtual_network_subnets = [{ cidr = "10.140.0.0/16", name = "aks" }]
 }
 
 module "vnet" {
-  source = "git@github.com:smsilva/azure-network.git//src/vnet?ref=main"
+  source = "git@github.com:smsilva/azure-network.git//src/vnet?ref=1.1.0"
 
-  platform_instance_name = var.platform_instance_name
-  location               = local.location
   name                   = local.virtual_network_name
   cidrs                  = local.virtual_network_cidrs
   subnets                = local.virtual_network_subnets
+  location               = local.location
 }
 
 module "aks" {
   source = "../../src"
 
-  platform_instance_name  = var.platform_instance_name
-  cluster_location        = "centralus"
+  cluster_name            = local.name
+  cluster_location        = local.location
   cluster_version         = "1.21.2"
   cluster_subnet_id       = module.vnet.subnets["aks"].instance.id
   cluster_admin_group_ids = local.admin_group_ids
-  default_node_pool_name  = "sbxsyspool" # 12 Alphanumeric characters
+  default_node_pool_name  = "system" # 12 Alphanumeric characters
 }
 
 output "aks_id" {
--- src/provider.tf
+++ src/provider.tf
@@ -5,8 +5,6 @@ provider "azurerm" {
 terraform {
   required_version = ">= 0.15.0, < 2.0.0"
 
-  backend "local" {}
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
