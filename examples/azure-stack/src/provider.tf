d00bb6c39e43facabb93e41081dfcf33fd084a79
Silvio Silva <smsilva@gmail.com>
2021-11-13 10:02:56 -0300

    WIP

--- examples/azure-stack/src/provider.tf
+++ examples/azure-stack/src/provider.tf
@@ -3,14 +3,5 @@ provider "azurerm" {
 }
 
 terraform {
-  required_version = ">= 0.15.0, < 2.0.0"
-
   backend "local" {}
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 2.72.0, <= 2.74.0"
-    }
-  }
 }
