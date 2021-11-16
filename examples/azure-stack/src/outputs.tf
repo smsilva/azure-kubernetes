b540cbca1ca6201adf20d266599a8ee9d3ff7140
Silvio Silva <smsilva@gmail.com>
2021-11-16 16:48:10 -0300

    refactor(outputs): new outputs

--- examples/azure-stack/src/outputs.tf
+++ examples/azure-stack/src/outputs.tf
@@ -1,24 +1,12 @@
-#output "aks_id" {
-#  value = module.aks.aks_id
-#}
-#
-#output "aks_kubelet_identity_client_id" {
-#  value = module.aks.aks_kubelet_identity_client_id
-#}
-#
-#output "aks_kubeconfig" {
-#  value     = module.aks.aks_kubeconfig
-#  sensitive = true
-#}
-
-output "cluster_name" {
-  value = local.cluster_name
+output "aks_id" {
+  value = module.aks.aks_id
 }
 
-output "vnet_id" {
-  value = module.vnet.id
+output "aks_kubelet_identity_client_id" {
+  value = module.aks.aks_kubelet_identity_client_id
 }
 
-output "virtual_network_name" {
-  value = local.virtual_network_name
+output "kubeconfig" {
+  value     = module.aks.aks_kubeconfig
+  sensitive = true
 }
