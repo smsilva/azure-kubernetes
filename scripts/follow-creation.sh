56d510550c8368ee466df8f2bf18ba13c70a5f14
Silvio Silva <smsilva@gmail.com>
2022-07-14 12:35:38 -0300

    refactor(scripts/follow-creation.sh): Update ArgoCD url test

--- scripts/follow-creation.sh
+++ scripts/follow-creation.sh
@@ -1,31 +1,21 @@
-kubectl get namespaces | grep -vE "^kube-|^default"
+kubectl get namespaces | grep -vE "^kube-|^default"; echo
 
-echo
-
-helm list -A
-
-echo
+helm list -A; echo
 
 kubectl get service \
   --namespace ingress-nginx \
   --ignore-not-found \
-| grep -E "NAME|LoadBalancer"
-
-echo
+| grep -E "NAME|LoadBalancer"; echo
 
 kubectl get ExternalSecrets \
   --all-namespaces \
   --ignore-not-found \
-| head -n 5
-
-echo
+| head -n 5; echo
 
 kubectl get Ingress \
   --all-namespaces \
   --ignore-not-found \
-| head -n 5
-
-echo
+| head -n 5; echo
 
 ARGOCD_HOST=$(kubectl \
   --namespace argocd \
@@ -34,27 +24,50 @@ ARGOCD_HOST=$(kubectl \
   --output jsonpath='{.items[0].spec.rules[0].host}' \
   --ignore-not-found)
 
+check_if_dns_is_resolvable_using_google() {
+  dig @8.8.8.8 "${ARGOCD_HOST}" | grep "^${ARGOCD_HOST}" > /dev/null
+}
+
+check_if_dns_is_resolvable_locally() {
+  dig ${ARGOCD_HOST} | grep "^${ARGOCD_HOST}" > /dev/null
+}
+
+argocd_url_test_using_curl() {
+  TEMP_FILE=$(tempfile)
+  URL="$(printf "https://%s" ${ARGOCD_HOST})"
+  printf "[curl] %s: " ${URL}
+  curl -Isk --max-time 3 ${URL} > "${TEMP_FILE}"
+
+  if [ $? -eq 0 ]; then
+    cat "${TEMP_FILE}" | head -1
+  else
+    echo "failed with an non HTTP code ($?)"
+  fi
+}
+
 if [ -n "${ARGOCD_HOST}" ]; then
-  dig ${ARGOCD_HOST} | grep "^${ARGOCD_HOST}"
-  
-  echo
+  check_if_dns_is_resolvable_using_google
 
-  if [ ! $? ]; then
-    dig @8.8.8.8 "${ARGOCD_HOST}" | grep "^${ARGOCD_HOST}"
-
-    if [ ! $? ]; then
-      echo "DNS not resolved by 8.8.8.8 yet: ${ARGOCD_HOST}"
+  if [ $? -eq 0 ]; then
+    check_if_dns_is_resolvable_locally
+    
+    if [ $? -eq 0 ]; then
+      argocd_url_test_using_curl
+    else
+      echo "DNS not resolved locally yet: ${ARGOCD_HOST}"
     fi
   else
-    echo "https://${ARGOCD_HOST}: $(curl -Isk --max-time 5 https://${ARGOCD_HOST} | head -1)"
+    echo "DNS not resolved by 8.8.8.8 yet: ${ARGOCD_HOST}"
   fi
+
+  echo
+
+  kubectl get Applications \
+    --namespace argocd \
+    --ignore-not-found \
+  | grep -E "NAME|bootstrap|infra|bar|nri|httpbin"
+
 else
-  echo "ArgoCD Ingress not created yet."
+  echo
+  echo "ArgoCD's Ingress not created yet."
 fi
-
-echo
-
-kubectl get Applications \
-  --namespace argocd \
-  --ignore-not-found \
-| grep -E "NAME|bootstrap|infra|bar|nri|httpbin"
