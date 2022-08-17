225b3b40e72acf8f1474ff68580e41454f6915e6
Silvio Silva <smsilva@gmail.com>
2022-08-17 11:02:29 -0300

    feat(docker/): New docker image

--- docker/api-access/build.sh
+++ docker/api-access/build.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
-export KUBECTL_VERSION="v${1-1.21.12}"
-
+export KUBECTL_VERSION="${1-1.22.12}"
 export DOCKERBUILD_CONTEXT_TEMPORARY_DIRECTORY="docker_build_temp"
 export KUBECTL_DOWNLOADED_BINARY="${DOCKERBUILD_CONTEXT_TEMPORARY_DIRECTORY?}/kubectl_downloaded_binary"
 
 mkdir -p "${DOCKERBUILD_CONTEXT_TEMPORARY_DIRECTORY?}"
 
-if [ -z "${KUBECTL_DOWNLOADED_BINARY?}" ]; then
-  curl \
-    --silent \
-    --location \
-    --output "${KUBECTL_DOWNLOADED_BINARY?}" \
-    --remote-name https://dl.k8s.io/release/v1.24.0/bin/linux/amd64/kubectl
+if ! [ -e "${KUBECTL_DOWNLOADED_BINARY?}" ]; then
+  wget \
+    --output-document ${KUBECTL_DOWNLOADED_BINARY?} \
+    https://dl.k8s.io/release/v${KUBECTL_VERSION?}/bin/linux/amd64/kubectl
 fi
 
 echo "kubectl:"
