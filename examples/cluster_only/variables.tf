69ba529cdc26b76c2e792589ed93b4e24b746a3d
Silvio Silva <smsilva@gmail.com>
2022-09-06 17:14:53 -0300

    refactor: NGINX Ingress Controller Example

--- examples/cluster_only/variables.tf
+++ examples/cluster_only/variables.tf
@@ -1,9 +1,9 @@
 data "azurerm_client_config" "current" {}
 
 resource "random_string" "id" {
-  length      = 6
-  min_lower   = 3
-  min_numeric = 3
+  length      = 4
+  min_lower   = 2
+  min_numeric = 2
   lower       = true
   special     = false
 }
