fddac5fd2305d586ebdae86cd6e5f96ec9e3fee2
Silvio Silva <smsilva@gmail.com>
2022-10-05 14:47:37 -0300

    refactor: Update examples using symbolic links

--- examples/cluster_with_argocd_ingress_azure/secrets.tf
+++
@@ -1,7 +0,0 @@
-module "argocd_app_registration_password" {
-  source = "git@github.com:smsilva/azure-key-vault.git//src/secret?ref=1.0.0"
-
-  vault = data.azurerm_key_vault.default
-  key   = "argocd-oidc-azuread-${module.argocd_app_registration.instance.application_id}"
-  value = module.argocd_app_registration.password
-}
