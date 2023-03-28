9cef355cad65a6c05605c57cf9dd2ffe8e9fe2bd
Silvio Silva <smsilva@gmail.com>
2023-03-28 08:54:08 -0300

    refactor: metrics-server removal

--- src/metrics-server/main.tf
+++
@@ -1,7 +0,0 @@
-resource "helm_release" "metrics-server" {
-  chart            = "${path.module}/../helm/charts/metrics-server"
-  name             = "metrics-server"
-  namespace        = "metrics-server"
-  create_namespace = true
-  atomic           = true
-}
